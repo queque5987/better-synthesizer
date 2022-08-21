f17a706fd1d7b59d58dad0fa645246c2d44ec42a
queque5987 <pyw5987@gmail.com>
2022-08-22 03:49:11 +0900

    20220822 0349

--- synthesizer/preprocess.py
+++ synthesizer/preprocess.py
@@ -2,7 +2,7 @@ from multiprocessing.pool import Pool
 from synthesizer import audio
 from functools import partial
 from itertools import chain
-from encoder import inference as encoder
+# from encoder import inference as encoder
 from pathlib import Path
 from utils import logmmse
 from tqdm import tqdm
@@ -181,61 +181,61 @@ def split_on_silences(wav_fpath, words, end_times, hparams):
     return wavs, texts
 
 
-def process_utterance(wav: np.ndarray, text: str, out_dir: Path, basename: str,
-                      skip_existing: bool, hparams):
-    ## FOR REFERENCE:
-    # For you not to lose your head if you ever wish to change things here or implement your own
-    # synthesizer.
-    # - Both the audios and the mel spectrograms are saved as numpy arrays
-    # - There is no processing done to the audios that will be saved to disk beyond volume
-    #   normalization (in split_on_silences)
-    # - However, pre-emphasis is applied to the audios before computing the mel spectrogram. This
-    #   is why we re-apply it on the audio on the side of the vocoder.
-    # - Librosa pads the waveform before computing the mel spectrogram. Here, the waveform is saved
-    #   without extra padding. This means that you won't have an exact relation between the length
-    #   of the wav and of the mel spectrogram. See the vocoder data loader.
+# def process_utterance(wav: np.ndarray, text: str, out_dir: Path, basename: str,
+#                       skip_existing: bool, hparams):
+#     ## FOR REFERENCE:
+#     # For you not to lose your head if you ever wish to change things here or implement your own
+#     # synthesizer.
+#     # - Both the audios and the mel spectrograms are saved as numpy arrays
+#     # - There is no processing done to the audios that will be saved to disk beyond volume
+#     #   normalization (in split_on_silences)
+#     # - However, pre-emphasis is applied to the audios before computing the mel spectrogram. This
+#     #   is why we re-apply it on the audio on the side of the vocoder.
+#     # - Librosa pads the waveform before computing the mel spectrogram. Here, the waveform is saved
+#     #   without extra padding. This means that you won't have an exact relation between the length
+#     #   of the wav and of the mel spectrogram. See the vocoder data loader.
 
 
-    # Skip existing utterances if needed
-    mel_fpath = out_dir.joinpath("mels", "mel-%s.npy" % basename)
-    wav_fpath = out_dir.joinpath("audio", "audio-%s.npy" % basename)
-    if skip_existing and mel_fpath.exists() and wav_fpath.exists():
-        return None
+#     # Skip existing utterances if needed
+#     mel_fpath = out_dir.joinpath("mels", "mel-%s.npy" % basename)
+#     wav_fpath = out_dir.joinpath("audio", "audio-%s.npy" % basename)
+#     if skip_existing and mel_fpath.exists() and wav_fpath.exists():
+#         return None
 
-    # Trim silence
-    if hparams.trim_silence:
-        wav = encoder.preprocess_wav(wav, normalize=False, trim_silence=True)
+#     # Trim silence
+#     if hparams.trim_silence:
+#         wav = encoder.preprocess_wav(wav, normalize=False, trim_silence=True)
 
-    # Skip utterances that are too short
-    if len(wav) < hparams.utterance_min_duration * hparams.sample_rate:
-        return None
+#     # Skip utterances that are too short
+#     if len(wav) < hparams.utterance_min_duration * hparams.sample_rate:
+#         return None
 
-    # Compute the mel spectrogram
-    mel_spectrogram = audio.melspectrogram(wav, hparams).astype(np.float32)
-    mel_frames = mel_spectrogram.shape[1]
+#     # Compute the mel spectrogram
+#     mel_spectrogram = audio.melspectrogram(wav, hparams).astype(np.float32)
+#     mel_frames = mel_spectrogram.shape[1]
 
-    # Skip utterances that are too long
-    if mel_frames > hparams.max_mel_frames and hparams.clip_mels_length:
-        return None
+#     # Skip utterances that are too long
+#     if mel_frames > hparams.max_mel_frames and hparams.clip_mels_length:
+#         return None
 
-    # Write the spectrogram, embed and audio to disk
-    np.save(mel_fpath, mel_spectrogram.T, allow_pickle=False)
-    np.save(wav_fpath, wav, allow_pickle=False)
+#     # Write the spectrogram, embed and audio to disk
+#     np.save(mel_fpath, mel_spectrogram.T, allow_pickle=False)
+#     np.save(wav_fpath, wav, allow_pickle=False)
 
-    # Return a tuple describing this training example
-    return wav_fpath.name, mel_fpath.name, "embed-%s.npy" % basename, len(wav), mel_frames, text
+#     # Return a tuple describing this training example
+    # return wav_fpath.name, mel_fpath.name, "embed-%s.npy" % basename, len(wav), mel_frames, text
 
 
-def embed_utterance(fpaths, encoder_model_fpath):
-    if not encoder.is_loaded():
-        encoder.load_model(encoder_model_fpath)
+# def embed_utterance(fpaths, encoder_model_fpath):
+#     if not encoder.is_loaded():
+#         encoder.load_model(encoder_model_fpath)
 
-    # Compute the speaker embedding of the utterance
-    wav_fpath, embed_fpath = fpaths
-    wav = np.load(wav_fpath)
-    wav = encoder.preprocess_wav(wav)
-    embed = encoder.embed_utterance(wav)
-    np.save(embed_fpath, embed, allow_pickle=False)
+#     # Compute the speaker embedding of the utterance
+#     wav_fpath, embed_fpath = fpaths
+#     wav = np.load(wav_fpath)
+#     wav = encoder.preprocess_wav(wav)
+#     embed = encoder.embed_utterance(wav)
+#     np.save(embed_fpath, embed, allow_pickle=False)
 
 
 def create_embeddings(synthesizer_root: Path, encoder_model_fpath: Path, n_processes: int):
