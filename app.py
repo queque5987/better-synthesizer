ea7a8f564d2333401d9cc4a186b0397747fae13a
queque5987 <pyw5987@gmail.com>
2022-08-30 19:35:19 +0900

    20220830 1935

--- app.py
+++ app.py
@@ -2,11 +2,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse, FileResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
+import json
 
 import rtvc_main
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class UserInput(BaseModel):
     embed: list
@@ -16,11 +26,15 @@ class UserInput(BaseModel):
 def index():
     return FileResponse("index.html")
 
-@app.get('/inference/')
+@app.post('/inference/')
 async def inference(userinput: UserInput):
     userinput = userinput.dict()
     embed = np.array(userinput["embed"])
     text = userinput["text"]
     spec = rtvc_main.inference(embed, text)
-    spec = jsonable_encoder(spec.tolist())
-    return JSONResponse(spec)
+    # spec = jsonable_encoder(spec.tolist())
+    spec = spec.tolist()
+    spec_json = json.dumps({
+        "spec": spec
+    })
+    return JSONResponse(spec_json)
